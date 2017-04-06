a1a6f58b5785eebf0ddac656e397fa91f8bb2e80
gaomneng <menggao@wisedu.com>
2017-04-06 10:22:30 +0800

    补助申请-添加测试数据

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -10,13 +10,13 @@
             <mt-tab-container-item id="1">
                 <span class="sectionHeader">国家困难补助</span>
 
-                <div class="awardItem" v-for="n in 3" @click="goToHelpDetail">
+                <div class="awardItem" v-for="item in BZList" @click="goToHelpDetail">
                     <div class="awardItemLeft">
                         <div class="line">
-                            <span class="level">一等奖</span><span class="money">¥8000</span><span class="peopleCount">200人已申请</span>
+                            <span class="level">{{item.KNBZDJDM}}</span><span class="money">¥8000</span><span class="peopleCount">200人已申请</span>
                         </div>
                         <div class="line">
-                            <span class="date">开放时间</span><span class="dateToDate">05.12--08.25</span><span class="markImage"><img src="../../../static/images/noApply.png" class="mark"></span>
+                            <span class="date">开放时间</span><span class="dateToDate">{{item.SQKSRQ}}-{{item.SQJSRQ}}</span><span class="markImage" v-show="item.SFKSQ"><img src="../../../static/images/noApply.png" class="mark"></span>
                         </div>
                     </div>
                     <!--<div class="awardItemRight">-->
@@ -176,10 +176,95 @@
 
 </style>
 <script type="text/javascript">
-    import {Navbar, TabItem, TabContainer, TabContainerItem, Cell} from 'mint-ui';
+    import {Navbar, TabItem, TabContainer, TabContainerItem, Cell, Indicator} from 'mint-ui';
     import myApply from '../components/myApply.vue'
     export default {
         created() {
+            var param = {IDENTITY_ID:'01294'};
+            Indicator.open('正在加载...');
+            this.$http.get('测试环境域名/sys/knbzapp/mobilePoorStuApply/getStuAllPoorType.do',param).then(res=>{
+                var data =[ {
+                    KNBZDM:'',
+                    KNBZMC:'',
+                    KNBZDJDM:'二等奖',
+                    DJMC:'',
+                    PDXN:'',
+                    PDXQ:'',
+                    JE:'',
+                    SQKSRQ:'2017/11/11',
+                    SQJSRQ:'2017/12/11',
+                    SFKSQ:true,
+                    YSQRS:''
+                },{
+                    KNBZDM:'',
+                    KNBZMC:'',
+                    KNBZDJDM:'一等奖',
+                    DJMC:'',
+                    PDXN:'',
+                    PDXQ:'',
+                    JE:'',
+                    SQKSRQ:'2017/11/11',
+                    SQJSRQ:'2017/12/11',
+                    SFKSQ:false,
+                    YSQRS:''
+                },{
+                    KNBZDM:'',
+                    KNBZMC:'',
+                    KNBZDJDM:'三等奖',
+                    DJMC:'',
+                    PDXN:'',
+                    PDXQ:'',
+                    JE:'',
+                    SQKSRQ:'2017/11/11',
+                    SQJSRQ:'2017/12/11',
+                    SFKSQ:false,
+                    YSQRS:''
+                }];
+
+                this.BZList = data;
+
+            },e=>{
+                Indicator.close();
+                var data =[ {
+                    KNBZDM:'',
+                    KNBZMC:'',
+                    KNBZDJDM:'二等奖',
+                    DJMC:'',
+                    PDXN:'',
+                    PDXQ:'',
+                    JE:'',
+                    SQKSRQ:'2017/11/11',
+                    SQJSRQ:'2017/12/11',
+                    SFKSQ:true,
+                    YSQRS:''
+                },{
+                    KNBZDM:'',
+                    KNBZMC:'',
+                    KNBZDJDM:'一等奖',
+                    DJMC:'',
+                    PDXN:'',
+                    PDXQ:'',
+                    JE:'',
+                    SQKSRQ:'2017/11/11',
+                    SQJSRQ:'2017/12/11',
+                    SFKSQ:false,
+                    YSQRS:''
+                },{
+                    KNBZDM:'',
+                    KNBZMC:'',
+                    KNBZDJDM:'三等奖',
+                    DJMC:'',
+                    PDXN:'',
+                    PDXQ:'',
+                    JE:'',
+                    SQKSRQ:'2017/11/11',
+                    SQJSRQ:'2017/12/11',
+                    SFKSQ:true,
+                    YSQRS:''
+                }];
+                this.BZList = data;
+
+            });
         },
         methods:{
             goToHelpDetail:function () {
@@ -189,7 +274,8 @@
         data(){
             return {
                 selected: "1",
-                msg: [1,2,3]
+                msg: [1,2,3],
+                BZList:[]
 
             }
         },
@@ -199,6 +285,7 @@
             [TabContainer.name]: TabContainer,
             [TabContainerItem.name]: TabContainerItem,
             [Cell.name]: Cell,
+            [Indicator.name]: Indicator,
             myApply
         },
         watch: {}
